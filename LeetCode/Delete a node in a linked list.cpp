a511694b5fe718f558736cd9b5350b6db08503a6
pratikwadekar88 <85816091+pratikwadekar88@users.noreply.github.com>
2024-09-04 22:33:56 +0530

    Create Delete a node in a linked list.cpp

---
+++ LeetCode/Delete a node in a linked list.cpp
@@ -0,0 +1,63 @@
+/*There is a singly-linked list head and we want to delete a node node in it.
+
+You are given the node to be deleted node. You will not be given access to the first node of head.
+
+All the values of the linked list are unique, and it is guaranteed that the given node node is not the last node in the linked list.
+
+Delete the given node. Note that by deleting the node, we do not mean removing it from memory. We mean:
+
+The value of the given node should not exist in the linked list.
+The number of nodes in the linked list should decrease by one.
+All the values before node should be in the same order.
+All the values after node should be in the same order.
+Custom testing:
+
+For the input, you should provide the entire linked list head and the node to be given node. node should not be the last node of the list and should be an actual node in the list.
+We will build the linked list and pass the node to your function.
+The output will be the entire list after calling your function.
+ 
+
+Example 1:
+
+
+Input: head = [4,5,1,9], node = 5
+Output: [4,1,9]
+Explanation: You are given the second node with value 5, the linked list should become 4 -> 1 -> 9 after calling your function.*/
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+    // Function to delete a node in a singly linked list, given only access to that node
+    void deleteNode(ListNode* node) {
+        
+        // Traverse the list until reaching the second to last node
+        while (node->next->next) {
+            // Copy the value of the next node into the current node
+            node->val = node->next->val;
+            // Move to the next node
+            node = node->next;
+        }
+        
+        // Copy the value of the last node into the second to last node
+        node->val = node->next->val;
+        // Remove the last node by setting the next pointer to NULL
+        node->next = NULL;
+    }
+};
